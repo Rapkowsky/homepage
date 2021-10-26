b9fbe03a2ccb0ea88915c947d2f3229fc3dc5da4
Rapkowsky <robertrapkowski19@gmail.com>
2021-10-26 20:50:40 +0200

    - class name changed - js nomenclature changed

--- js/script.js
+++ js/script.js
@@ -1,45 +1,46 @@
-const welcome = () => {
-	console.log("Witam wszystkich, którzy tu zajrzeli!");
-};
-const onChangeBackgroundClick = () => {
-	articleElement.classList.toggle("light-dark");
-	bodyElement.classList.toggle("dark");
-	mainElement.classList.toggle("dark");
-	tableHeaderElement.classList.toggle("main-theme");
-	tableHeaderElement2.classList.toggle("main-theme");
-	tableHeaderElement3.classList.toggle("main-theme");
-	tableHeaderElement4.classList.toggle("main-theme");
-	tableElement.classList.toggle("light-dark");
-	if (bodyElement.classList.contains("dark")) {
-		themeElement.innerHTML = "Jasny";
-	} else {
-		themeElement.innerHTML = "Ciemny";
-	}
-};
+{
+	const welcome = () => {
+		console.log("Witam wszystkich, którzy tu zajrzeli!");
+	};
+	const onChangeBackgroundClick = () => {
+		const bodyElement = document.querySelector(".js-body");
+		const articleElement = document.querySelector(".js-article");
+		const mainElement = document.querySelector(".js-main");
+		const tableElement = document.querySelector(".js-table");
+		const tableHeaderElement = document.querySelector(".table__cell--header-color");
+		const tableHeaderElement2 = document.querySelector(".js-header-2");
+		const tableHeaderElement3 = document.querySelector(".js-header-3");
+		const tableHeaderElement4 = document.querySelector(".js-header-4");
+		const themeElement = document.querySelector(".navigation__theme");
 
-welcome();
+		articleElement.classList.toggle("js-light-dark");
+		bodyElement.classList.toggle("js-dark");
+		mainElement.classList.toggle("js-dark");
+		tableHeaderElement.classList.toggle("js-main-theme");
+		tableHeaderElement2.classList.toggle("js-main-theme");
+		tableHeaderElement3.classList.toggle("js-main-theme");
+		tableHeaderElement4.classList.toggle("js-main-theme");
+		tableElement.classList.toggle("js-light-dark");
+		if (bodyElement.classList.contains("js-dark")) {
+			themeElement.innerHTML = "Jasny";
+		} else {
+			themeElement.innerHTML = "Ciemny";
+		}
+	};
+	const darkModeButtonElement = document.querySelector(".dark-mode");
+	const buttonElement = document.querySelector(".box__button");
+	const mainImage = document.querySelector(".box__circle");
+	darkModeButtonElement.addEventListener("click", onChangeBackgroundClick);
 
-let buttonElement = document.querySelector(".box__button");
-let mainImage = document.querySelector(".box__circle");
-let darkModeButtonElement = document.querySelector(".dark-mode");
-let bodyElement = document.querySelector(".body");
-let articleElement = document.querySelector("article");
-let mainElement = document.querySelector(".main");
-let tableElement = document.querySelector(".table-section");
-let tableHeaderElement = document.querySelector(".table__cell--header-color");
-let tableHeaderElement2 = document.querySelector(".table__cell--header-color2");
-let tableHeaderElement3 = document.querySelector(".table__cell--header-color3");
-let tableHeaderElement4 = document.querySelector(".table__cell--header-color4");
-let themeElement = document.querySelector(".navigation__theme");
+	buttonElement.addEventListener("click", () => {
+		if (mainImage.classList.contains("js-visible")) {
+			mainImage.classList.remove("js-visible");
+			buttonElement.innerHTML = "Usuń zdjęcie";
+		} else {
+			mainImage.classList.add("js-visible");
+			buttonElement.innerHTML = "Pokaż zdjęcie";
+		}
+	});
 
-darkModeButtonElement.addEventListener("click", onChangeBackgroundClick);
-
-buttonElement.addEventListener("click", () => {
-	if (mainImage.classList.contains("visible")) {
-		mainImage.classList.remove("visible");
-		buttonElement.innerHTML = "Usuń zdjęcie";
-	} else {
-		mainImage.classList.add("visible");
-		buttonElement.innerHTML = "Pokaż zdjęcie";
-	}
-});
+	welcome();
+}
